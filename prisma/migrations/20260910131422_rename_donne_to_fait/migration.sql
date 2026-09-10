3f8a5bfbd4cc4cffd41c7368bcb5abb5f2016596
Alexis <64962746+alexis-nauleau@users.noreply.github.com>
2026-09-10 17:11:25 +0200

    chore: vide la branche master

--- prisma/migrations/20260910131422_rename_donne_to_fait/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `donne` on the `Taches` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Taches" DROP COLUMN "donne",
-ADD COLUMN     "Fait" BOOLEAN NOT NULL DEFAULT false;
